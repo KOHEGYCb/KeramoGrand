af891616cac90b7741c3bf82425dd40ac8eb78c2
Unknown <dimaushenko@mail.ru>
2019-12-17 02:49:53 +0300

    start modeling menu

---
+++ test/js.js
@@ -0,0 +1,21 @@
+function openMenu() {
+    if (isMenuOpen) {
+        document.getElementById("nav").style.top = -1000;
+        isMenuOpen = !isMenuOpen;
+    } else {
+        document.getElementById("nav").style.height = document.body.clientHeight - 71 - 60;
+        document.getElementById("nav").style.top = 71;
+        isMenuOpen = !isMenuOpen;
+    }
+}
+
+function toogleKol() {
+    
+}
+
+var isMenuOpen = false;
+var btn_addSQLCol = document.getElementById('btn_menu');
+btn_addSQLCol.addEventListener('click', openMenu, false);
+
+var btnKol = document.getElementById('kollections');
+btnKol.addEventListener('click', toogleKol, false);
