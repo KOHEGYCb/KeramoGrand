5ff51c478524aa37c51daa0c04ba97cd85b01c92
Unknown <dimaushenko@mail.ru>
2019-12-23 15:44:33 +0300

    html for gallery

--- test/gallery.js
+++ test/gallery.js
@@ -1,11 +1,23 @@
 function resizeGallery(){
+	var amount = 1;
 	var galleryWidth = document.documentElement.clientWidth;
 
 	var galleryElements = document.getElementById('gallery').children;
 
+
+	if (galleryWidth > 700){
+		amount++;
+	}
+	if (galleryWidth > 1050){
+		amount++;
+	}
+	if (galleryWidth > 1400){
+		amount++;
+	}
+
 	for (var i = galleryElements.length - 1; i >= 0; i--) {
-		galleryElements[i].style.width = galleryWidth/3 + "px";
-		galleryElements[i].style.height = galleryWidth/9 + "px";
+		galleryElements[i].style.width = galleryWidth/amount + "px";
+		galleryElements[i].style.height = galleryWidth/(amount * 3) + "px";
 	}
 }
 
@@ -13,3 +25,15 @@ resizeGallery();
 $(window).on('resize', function () {
     resizeGallery();
 });
+
+var galleryElements = document.getElementById('gallery').children;
+for (var i = galleryElements.length - 1; i >= 0; i--) {
+	if (galleryElements[i].getAttribute('color') === "w"){
+		galleryElements[i].style.color = "#d6d6d6";
+		galleryElements[i].style.borderColor = "#d6d6d6"
+	}
+	if (galleryElements[i].getAttribute('color') === "b"){
+		galleryElements[i].style.color = "#201f24"
+		galleryElements[i].style.borderColor = "#201f24"
+	}
+}
